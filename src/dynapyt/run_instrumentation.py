3fd231fd76c8eaea068bfe3fc6023dcf8636e030
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2022-07-24 11:52:42 +0200

    Updated Dockerfile and run script

--- src/dynapyt/run_instrumentation.py
+++ src/dynapyt/run_instrumentation.py
@@ -25,10 +25,12 @@ if __name__ == '__main__':
     all_cmds = []
     for dir_path, dir_names, file_names in walk(start):
         for name in file_names:
+            # if (name in ['setup.py', '__init__.py']) or ('config' in name):
+            #     continue
             if name.endswith('.py'):
                 file_path = path.join(dir_path, name)
                 cmd_list = ['python', '-m', 'dynapyt.instrument.instrument', '--files', file_path, '--analysis', analysis]
                 all_cmds.append((cmd_list, file_path))
     with Pool(maxtasksperchild=5) as p:
         p.starmap(process_files, all_cmds)
-    print('--------> ' + str(time.time() - start_time))
+    print('#################### Instrumentation took ' + str(time.time() - start_time))
