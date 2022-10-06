b5edcfa1387a97290901865b5dafee46784a8fb5
Michael Pradel <michael@binaervarianz.de>
2022-10-06 11:18:02 +0200

    Test case for if statements Fixes #1

---
+++ tests/trace_single_hook/if/analysis.py
@@ -0,0 +1,23 @@
+from typing import Any, Optional
+from dynapyt.analyses.BaseAnalysis import BaseAnalysis
+from dynapyt.instrument.IIDs import IIDs
+
+
+class TestAnalysis(BaseAnalysis):
+    def begin_execution(self) -> None:
+        print("begin execution")
+
+    def enter_control_flow(self, dyn_ast: str, iid: int, cond_value: bool) -> Optional[bool]:
+        print(f"enter control flow event with condition {cond_value}")
+
+    def enter_if(self, dyn_ast: str, iid: int, cond_value: bool) -> Optional[bool]:
+        print(f"if condition evaluates to {cond_value}")
+    
+    def exit_if(self, dyn_ast: str, iid: int):
+        print(f"done with if statement")
+
+    def exit_control_flow(self, dyn_ast: str, iid: int) -> None:
+        print(f"done with control flow statement")
+
+    def end_execution(self) -> None:
+        print("end execution")
---
+++ tests/trace_single_hook/if/program.py
@@ -0,0 +1,5 @@
+foo = 45 + 23
+if foo > 50:
+    print("foo is greater than 50")
+else:
+    print("foo is less than or equal to 50")
