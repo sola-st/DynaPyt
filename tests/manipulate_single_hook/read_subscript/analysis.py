cc587f64e384788162ca42b40635e19db0c163ff
Michael Pradel <michael@binaervarianz.de>
2022-11-25 11:28:33 +0100

    Code formatting

--- tests/manipulate_single_hook/read_subscript/analysis.py
+++ tests/manipulate_single_hook/read_subscript/analysis.py
@@ -3,7 +3,9 @@ from dynapyt.analyses.BaseAnalysis import BaseAnalysis
 
 
 class TestAnalysis(BaseAnalysis):
-    def read_subscript(self, dyn_ast: str, iid: int, base: Any, sl: List[Union[int, Tuple]], val: Any) -> Any:
+    def read_subscript(
+        self, dyn_ast: str, iid: int, base: Any, sl: List[Union[int, Tuple]], val: Any
+    ) -> Any:
         print(f"read from base {base} subscript {sl} gives value {val}")
         if type(val) == int:
             return val + 1
