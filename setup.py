37ac9d7335fe01575f58a85edc5be981aafa72cd
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-01-30 10:48:55 +0100

    Updated version for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynapyt",
-    version="0.2.1",
+    version="0.2.2",
     author="Aryaz Eghbali",
     author_email="aryaz.egh@gmail.com",
     description="Dynamic analysis framework for Python",
--- src/dynapyt/__init__.py
+++ src/dynapyt/__init__.py
@@ -1,2 +1,2 @@
 """DynaPyt"""
-__version__ = '0.2.1'
+__version__ = '0.2.2'
