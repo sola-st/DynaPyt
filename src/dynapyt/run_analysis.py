08f52feb4258d08e574f2e6d0f8edd6594e9e5c7
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-06-15 14:29:25 +0200

    Refactored to run instrumentation and analysis programmatically

--- src/dynapyt/run_analysis.py
+++ src/dynapyt/run_analysis.py
@@ -4,41 +4,25 @@ from os.path import abspath
 import sys
 import signal
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--entry", help="Entry file for execution")
-parser.add_argument(
-    "--analysis", help="Analysis class name")
-parser.add_argument(
-    "--module", help="Adds external module paths")
-parser.add_argument(
-    "--name", help="Associates a given name with current run"
-)
 
-import dynapyt.runtime as rt
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    additional_module = args.module
-    name = args.name
-    analysis = args.analysis
-    modulePath = 'dynapyt.analyses'
+def run_analysis(entry: str, analysis: str, module: str = None, name: str = None):
+    modulePath = "dynapyt.analyses"
     if additional_module is not None:
         modulePath = additional_module
     try:
-        module = importlib.import_module(modulePath + '.' + analysis)
+        module = importlib.import_module(modulePath + "." + analysis)
     except TypeError as e:
-        print(f'--module was used but no value specified {e}')
+        print(f"--module was used but no value specified {e}")
     except ImportError as e:
-        print(f'module could not be imported {e}')
+        print(f"module could not be imported {e}")
 
-    class_ = getattr(module, args.analysis)
+    class_ = getattr(module, analysis)
     my_analysis = class_()
     rt.set_analysis(my_analysis)
 
     def end_execution():
         try:
-            func = getattr(my_analysis, 'end_execution')
+            func = getattr(my_analysis, "end_execution")
             func()
         except AttributeError:
             pass
@@ -49,16 +33,32 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, end_execution)
 
     if not name is None:
-        getattr(my_analysis, 'add_metadata', lambda: None)({"name": name})
+        getattr(my_analysis, "add_metadata", lambda: None)({"name": name})
 
     try:
-        func = getattr(my_analysis, 'begin_execution')
+        func = getattr(my_analysis, "begin_execution")
         func()
     except AttributeError:
         pass
-    if args.entry.endswith('.py'):
-        sys.argv = [args.entry]
-        exec(open(abspath(args.entry)).read())
+    if entry.endswith(".py"):
+        sys.argv = [entry]
+        exec(open(abspath(entry)).read())
     else:
-        importlib.import_module(args.entry, '*')
+        importlib.import_module(entry, "*")
     end_execution()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--entry", help="Entry file for execution")
+parser.add_argument("--analysis", help="Analysis class name")
+parser.add_argument("--module", help="Adds external module paths")
+parser.add_argument("--name", help="Associates a given name with current run")
+
+import dynapyt.runtime as rt
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    additional_module = args.module
+    name = args.name
+    analysis = args.analysis
+    run_analysis(args.entry, analysis, additional_module, name)
--- src/dynapyt/run_instrumentation.py
+++ src/dynapyt/run_instrumentation.py
@@ -7,30 +7,12 @@ from subprocess import run
 import time
 from multiprocessing import Pool
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--directory", help="Directory of the project to analyze")
-parser.add_argument(
-    "--analysis", help="Analysis class name")
-parser.add_argument(
-    "--module", help="Adds external module paths")
-parser.add_argument(
-    "--external_dir", help="Place instrumented files in another directory",
-    dest='external_dir', action='store_true'
-)
 
-def process_files(cmd_list, file_path):
-    comp_proc = run(cmd_list)
-    if comp_proc.returncode != 0:
-        print('Error at', file_path)
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    start = args.directory
-    analysis = args.analysis
-    module = args.module
-    use_external_dir = args.external_dir
+def instrument_dir(
+    directory: str, analysis: str, module: str = None, use_external_dir: bool = False
+):
     start_time = time.time()
+    start = directory
     all_cmds = []
 
     if use_external_dir:
@@ -39,15 +21,50 @@ if __name__ == '__main__':
         shutil.rmtree(external_path, ignore_errors=True)
         shutil.copytree(start, external_path)
         start = str(external_path)
-    
+
     for dir_path, dir_names, file_names in walk(start):
         for name in file_names:
-            if name.endswith('.py'):
+            if name.endswith(".py"):
                 file_path = path.join(dir_path, name)
-                cmd_list = ['python', '-m', 'dynapyt.instrument.instrument', '--files', file_path, '--analysis', analysis]
+                cmd_list = [
+                    "python",
+                    "-m",
+                    "dynapyt.instrument.instrument",
+                    "--files",
+                    file_path,
+                    "--analysis",
+                    analysis,
+                ]
                 if module is not None:
-                    cmd_list.extend(['--module', module])
+                    cmd_list.extend(["--module", module])
                 all_cmds.append((cmd_list, file_path))
     with Pool(maxtasksperchild=5) as p:
         p.starmap(process_files, all_cmds)
-    print('#################### Instrumentation took ' + str(time.time() - start_time))
+    print("#################### Instrumentation took " + str(time.time() - start_time))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--directory", help="Directory of the project to analyze")
+parser.add_argument("--analysis", help="Analysis class name")
+parser.add_argument("--module", help="Adds external module paths")
+parser.add_argument(
+    "--external_dir",
+    help="Place instrumented files in another directory",
+    dest="external_dir",
+    action="store_true",
+)
+
+
+def process_files(cmd_list, file_path):
+    comp_proc = run(cmd_list)
+    if comp_proc.returncode != 0:
+        print("Error at", file_path)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    start = args.directory
+    analysis = args.analysis
+    module = args.module
+    use_external_dir = args.external_dir
+    instrument_dir(start, analysis, module, use_external_dir)
