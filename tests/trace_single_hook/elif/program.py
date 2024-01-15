ea0a8eb5c5d182db1dd1b005e51c242ac0d3fc9d
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-01-15 12:10:21 +0100

    Added more tests for elif

--- tests/trace_single_hook/elif/program.py
+++ tests/trace_single_hook/elif/program.py
@@ -7,3 +7,8 @@ elif foo == 50:
     print("foo is equal to 50")
 else:
     print("foo is less than 50")
+
+if foo % 3 == 0:
+    print("foo is divisible by 3")
+elif foo % 3 == 1:
+    print("foo is 1 more than a multiple of 3")
