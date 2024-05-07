c9a02d2dcb675ecfc0fbf65dcdf542a62dd0d82f
Keerthi <keerthivasudevan98@gmail.com>
2024-05-07 12:13:56 +0200

    Added tests in trace_single_hook for try

---
+++ tests/trace_single_hook/try/analysis.py
@@ -0,0 +1,11 @@
+from dynapyt.analyses.BaseAnalysis import BaseAnalysis
+
+class TestAnalysis(BaseAnalysis):
+    def begin_execution(self) -> None:
+        print("begin execution")
+
+    def enter_try(self, dyn_ast: str, iid: int) -> None:
+        print(f"entered try block")
+        
+    def end_execution(self) -> None:
+        print("end execution")
