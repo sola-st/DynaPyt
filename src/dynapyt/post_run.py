fc6ee78f47c67dfa288ec2519f4048d59a186a41
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-08-30 12:17:05 +0200

    Fixed post run

--- src/dynapyt/post_run.py
+++ src/dynapyt/post_run.py
@@ -5,8 +5,9 @@ from .utils.runtimeUtils import gather_coverage, gather_output
 
 
 def post_run(coverage_dir: str, output_dir: str):
-    gather_coverage(Path(coverage_dir))
     gather_output(Path(output_dir))
+    if len(coverage_dir) > 0:
+        gather_coverage(Path(coverage_dir))
 
 
 if __name__ == "__main__":
