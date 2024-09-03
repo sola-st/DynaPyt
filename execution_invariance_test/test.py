8673ba762fdfd0ae67be2487fc7e9c5ebd43ddea
Keerthi <keerthivasudevan98@gmail.com>
2024-09-03 16:31:06 +0530

    Added yml file in github workflows for execution invariance tests

--- execution_invariance_test/test.py
+++ execution_invariance_test/test.py
@@ -60,9 +60,7 @@ def run_instrumented_project(project):
     return test_result_map
 
 
-def remove_related_modules(project):
-    project_module = project.name.replace("-", "_")
-
+def remove_related_modules(project, project_module):
     project_src_dir = project / "src"
     for code_file in project_src_dir.rglob("*.py"): 
         module_name = project_module + "." + code_file.stem
@@ -78,16 +76,16 @@ def remove_related_modules(project):
             del sys.modules[module_name]
 
 
-def run_tests(projects_dir):
+def run_tests(projects, project_module_map):
     invariance_test_node_id = "tests/stack_test.py::test_stack"
     failed = False
     # Run the tests with and without instrumentation
-    for project in projects_dir.iterdir():
+    for project in projects:
         print(f"Installing project {project.name}")
         install_project(project)
         print(f"Running project {project.name}")
         test_result = run_project(project)
-        remove_related_modules(project)
+        remove_related_modules(project, project_module_map[project.name])
         instrumented_test_result = run_instrumented_project(project)
 
         for test, result in test_result.items():
@@ -106,6 +104,7 @@ def run_tests(projects_dir):
                 print(f"Test {test} results do not match")
                 failed = True
                 break
+        print("All tests passed for project", project.name)
         sys.path.remove(str(project / "src"))
         sys.path.remove(str(project.parent))
 
@@ -113,8 +112,6 @@ def run_tests(projects_dir):
         print("Test results do not match before and after instrumentation")
         return False
     
-    print(f"All tests passed for projects in folder {projects_dir}")
-
     return True
 
 
@@ -132,20 +129,23 @@ def run_test_on_projects():
     dynapyt_dir = Path(__file__).parent.parent
     execution_invariance_test_dir = Path(__file__).parent
     test_folders_file = execution_invariance_test_dir / "test_folders.txt"
+    projects = []
+    project_module_map = {}
     with open(test_folders_file, "r") as f:
         while True:
             line = f.readline()
             if not line:
                 break
-            projects_dir = dynapyt_dir / line.strip()
-            if not run_tests(projects_dir):
-                is_successful = False
-                break
+            project_dir, project_module = line.strip().split()
+            project = dynapyt_dir / project_dir
+            projects.append(project)
+            project_module_map[project.name] = project_module
 
-    if is_successful:
+    if run_tests(projects, project_module_map):
         print("All tests passed for all projects")
     else:
         print("Some tests failed")
+
     return is_successful
 
 
